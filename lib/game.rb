2c89216e06bdd0df15ce1642929962ff34fedb52
alexanderhulteng <alexanderhulteng@gmail.com>
2021-08-23 13:50:06 -0600

    commit tree 906e083664428d309fff4f48ea9a2a615ee3ae9b

---
+++ lib/game.rb
@@ -0,0 +1,80 @@
+class Game
+    attr_accessor :board, :player_1, :player_2
+    WIN_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+        @board = board
+        @player_1 = player_1
+        @player_2 = player_2
+    end
+
+    def current_player
+        @board.turn_count % 2 == 0 ? player_1 : player_2
+    end
+
+    def won?
+        WIN_COMBINATIONS.detect do |k|
+            @board.cells[k[0]] == @board.cells[k[1]] &&
+            @board.cells[k[1]] == @board.cells[k[2]] &&
+            (@board.cells[k[0]] == "X" || @board.cells[k[0]] == "O")
+        end
+    end
+
+    def draw?
+        @board.full? && !won?
+    end
+
+    def over?
+        won? || draw?
+    end
+
+    def winner
+        if winning_combo = won?
+            @winner = @board.cells[winning_combo.first]
+        end
+    end
+
+    def turn
+        
+        puts "Player #{current_player.token}'s turn!"
+        puts "Where would you like to move? (1-9):"
+        board.display
+    
+        input = current_player.move(board)
+    
+        if board.valid_move?(input)
+          board.update(input, current_player)
+        else
+          puts "Please enter a different selection."
+          turn
+        end
+    end
+
+    def play 
+        while !over?
+            turn
+        end
+        if won?
+            puts "Congratulations #{winner}!"
+          elsif draw?
+            puts "Cat's Game!"
+        end
+    end
+
+
+    
+
+
+
+
+end
+
---
+++ lib/players/human.rb
@@ -0,0 +1,11 @@
+module Players
+    class Human < Player
+
+        def move(board)
+            puts 'Enter your selection!'
+            gets.strip
+        end
+
+    end
+
+end
